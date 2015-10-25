7ac88f45d0df9a6922860d7fb9bb62c40aa79d1c
Mario Perez <mapreal19@gmail.com>
2015-10-25 23:56:41 +0100

    Blog Link

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,17 +4,6 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
