2133a8948c99c99e6bba041e82d00db40c7f3399
Mario Perez <mapreal19@gmail.com>
2016-07-06 22:39:52 +0200

    Concatenate js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+---
+---
+
+{% include_relative jquery-1.11.0.js %}
+{% include_relative bootstrap.js %}
+
+{% include_relative classie.js %}
+{% include_relative cbpAnimatedHeader.js %}
+
+{% include_relative freelancer.js %}
